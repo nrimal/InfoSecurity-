5127d29603c1a2f1750a7109c856ac04cbd54d1b
nrimal <rimal.5@osu.edu>
2018-11-08 11:52:57 -0500

    inital login condition added to access website

---
+++ passwordManager/frontend/src/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+const Home = () => <h2>Home</h2>;//show all the website that it has saved password from
+const Edit = () => <h2>About</h2>;//call component to edit exisiting website password
+const Add = () => <h2>Users</h2>;//call component to add new website(a forum)
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main className="container-fluid">
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/Edit' component={Edit} />
+                    <Route path='/Add' component={Add} />
+                </Switch>
+            </main>
+        );
+    }
+}
+
+export default Main
--- passwordManager/frontend/src/index.js
+++ passwordManager/frontend/src/index.js
@@ -2,9 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//here login the user using civic and see if they have an account and only then
+//allow them to go to App
+
+
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+  }
+  
+  function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+  }
+
+  function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return (
+          <div>
+      <UserGreeting />
+        <App />
+        </div>
+        );
+    }
+    return <GuestGreeting />;
+  }
+  
+function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    );
+  }
+  
+  function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    );
+  }
+
+class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClick.bind(this);
+      this.handleLogoutClick = this.handleLogoutClick.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+  
+    handleLoginClick() {
+      this.setState({isLoggedIn: true});
+    }
+  
+    handleLogoutClick() {
+      this.setState({isLoggedIn: false});
+    }
+  
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      let button;
+  
+      if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} /> ;
+      } else {
+        button = <LoginButton onClick={this.handleLoginClick} />;
+      }
+  
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          {button}
+        </div>
+      );
+    }
+  }
+
+ReactDOM.render(
+    <BrowserRouter>
+    <LoginControl/>
+</BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
